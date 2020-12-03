e10e2340c49d10c20814af21f9d9e7d23b695374
Dirk Agterhuis <dirkagterhuis@gmail.com>
2020-12-03 14:34:12 +0100

    don't render the whole map by using modulo

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 
-var mapMultiplier = 13;
 var input = new Array();
 var startCoordinateX = 0;
 var startCoordinateY = 0;
@@ -8,22 +7,19 @@ var startCoordinateY = 0;
 fs.readFileSync('./input.txt', 'utf-8')
     .split(/[\r\n]+/g)
     .forEach(element => {
-        toPush = element.split('');
-        for (i = 0; i < mapMultiplier - 1; i++) {
-            toPush.push(...toPush);
-        }
-        input.push(toPush);
+        input.push(element.split(''));
     });
 
-// Access first row, (vertical) then column (horizontal) or access [x,y]: x = vertical, y = horizontal
+// Access first row, (vertical) then column (horizontal) or access [x,y]: x = vertical, y = horizontal:
 // console.log(input[2][1]);
+// console.table(input);
 
 function puzzleOne(slopeRight, slopeDown) {
     let treeCount = 0;
     let y = startCoordinateY;
     for (x = startCoordinateX + slopeDown; x <= input.length - 1; x = x + slopeDown) {
         y = y + slopeRight;
-        if (input[x][y] == '#') {
+        if (input[x][y % input[x].length] == '#') {
             treeCount++
         }
     }
@@ -31,5 +27,4 @@ function puzzleOne(slopeRight, slopeDown) {
 }
 
 console.log('Puzzle One: # of trees = ' + puzzleOne(3,1));
-var puzzleTwo = puzzleOne(1, 1) * puzzleOne (3, 1) * puzzleOne(5, 1) * puzzleOne(7, 1) * puzzleOne(1, 2);
-console.log('Puzzle Two: ' + puzzleTwo);
+console.log('Puzzle Two: # of trees = ' + puzzleOne(1, 1) * puzzleOne (3, 1) * puzzleOne(5, 1) * puzzleOne(7, 1) * puzzleOne(1, 2));
