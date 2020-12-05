da7bb9b1d831acb9972f49adadf458d682ad24c4
Dirk Agterhuis <dirkagterhuis@gmail.com>
2020-12-05 23:47:56 +0100

    fix it & clean up

--- Day 5/app.js
+++ Day 5/app.js
@@ -2,18 +2,18 @@ var fs = require('fs');
 
 var numberOfRows = 128;
 var numberOfColums = 8;
-var input = new Array();
-var seats = new Array();
+var input = [];
+var seats = [];
+
 for (i = 0; i <= numberOfRows -1; i++) {
-    var row = new Array();
+    var row = [];
     for (j = 0; j <= numberOfColums -1; j++) {
         row.push('');
     }
     seats.push(row);
 }
 
-// console.table(seats);
-// Example input: 'FBFBBFFRLR'. Row: 44, Column: 5, seatId: 357
+// Example input: 'FBFBBFFRLR'. Row: 44, Column: 5, seatId: 44 * 8 + 5 = 357
 fs.readFileSync('./input.txt', 'utf-8')
     .split(/[\r\n]+/g)
     .forEach(element => {
@@ -24,16 +24,17 @@ function boardThePlane() {
     input.forEach(boardingPass => {
         var row = numberOfRows;
         var column = numberOfColums;
-        
-        powerRow = 6;
-        powerColumn = 2;
-        for (i = 0; i <= boardingPass.length; i++) {
-            var char = boardingPass.charAt(i);
-            if (char == 'F') { row = row - Math.pow(2, powerRow); }
-            if (['F','B'].includes(char) ) { powerRow--; }
-            if (char == 'L') { column = column - Math.pow(2, powerColumn); }
-            if (['L','R'].includes(char)) { powerColumn--; }
-        }
+        powerRow = Math.log(numberOfRows) / Math.log(2) - 1;
+        powerColumn = Math.log(numberOfColums) / Math.log(2) - 1;
+
+        boardingPass.split('')
+            .forEach(char => {
+                if (char == 'F') { row = row - Math.pow(2, powerRow); }
+                if (['F','B'].includes(char) ) { powerRow--; }
+                if (char == 'L') { column = column - Math.pow(2, powerColumn); }
+                if (['L','R'].includes(char)) { powerColumn--; }
+            });
+            
         rowIndex = row - 1;
         columnIndex = column - 1;
         var seatId = rowIndex * 8 + columnIndex;
@@ -42,20 +43,15 @@ function boardThePlane() {
     })
 }
 
-// ok this doesn't work yet. 
 function getMySeat() {
     var allSeats = [];
     var mySeat;
     seats.forEach(row => {
         allSeats = [...allSeats, ...row];
     });
-    console.log('allseats: ' + JSON.stringify(allSeats));
     for (i = 1; i <= allSeats.length - 1; i++) {
-        if (allSeats[i = ''] && allSeats[i - 1] != '' && allSeats[i + 1] != '') {
+        if (allSeats[i] == '' && allSeats[i - 1] != '' && allSeats[i + 1] != '') {
             mySeat = allSeats[i - 1] + 1;
-            console.log('I: ' + i);
-            console.log('I in ding: ' + allSeats[i]);
-            console.log('I in ding - 1: ' + allSeats[i - 1]);
             break;
         }
     }
@@ -64,6 +60,5 @@ function getMySeat() {
 
 boardThePlane();
 console.table(seats);
-var max = Math.max(...[].concat(...seats))
-console.log('The highest Seat Id is: ' + max);
-console.log('mySeat is: ' + getMySeat());
+console.log('The highest Seat Id is: ' + Math.max(...[].concat(...seats))); // should be 894
+console.log('My seat is: ' + getMySeat()); // should be 579
