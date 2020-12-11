0ea9bd01cb8396327a327000492ef80d01733eec
Dirk Agterhuis <dirkagterhuis@gmail.com>
2020-12-11 17:13:27 +0100

    renaming directories

---
+++ Day 11 - Boat seating system/app.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+var input = [];
+fs.readFileSync('./input.txt', 'utf-8')
+    .trim()
+    .replace(/\r\n/g, '\n')
+    .split(/\n/g)
+    .forEach(element => {
+        var split = element.split(' ');
+        input.push({ 
+
+        });
+    })
