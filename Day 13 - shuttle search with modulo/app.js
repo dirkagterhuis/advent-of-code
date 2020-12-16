7a5cfe1376113e9aa7f66cb6d006f7bef58d2fc6
Dirk Agterhuis <dirkagterhuis@gmail.com>
2020-12-16 16:51:34 +0100

    changing devices so committing

--- Day 13 - shuttle search with modulo/app.js
+++ Day 13 - shuttle search with modulo/app.js
@@ -1,15 +1,20 @@
 const { time } = require('console');
 var fs = require('fs');
 
-var split = fs.readFileSync('./input.txt', 'utf-8').split(/[\r\n]+/g);
+var split = fs.readFileSync('./inputExample.txt', 'utf-8').split(/[\r\n]+/g);
 var timeToCatchTheBus = split[0];
 var busDurations = split[1]
     .replace(/,x/g, '')
     .split(',')
     .map((i) => Number(i));
+var busDurationsWithX = split[1].split(',');
+busDurationsWithX.forEach(duration => {
+    if (duration !== 'x') { duration = parseInt(duration); }
+});
 
 console.log('timeToCatchTheBus: ' + timeToCatchTheBus);
 console.log('busDurations: ' + busDurations);
+console.log('busDurationsWithX: ' + busDurationsWithX);
 
 var getMyBus = () => {
     var busByNextDepart = [];
@@ -34,3 +39,23 @@ var getMyBus = () => {
 }
 
 console.log('Puzzle 1: ' + getMyBus()); // 5257 or 295 in example
+console.log('***');
+
+//forget it
+var puzzleTwo = () => {
+    var found = false;
+    allNumbers:
+    for (i = 1; !found; i++) {
+        console.log('i: ' + i)
+        for (j = 0; j <= busDurationsWithX.length; j++) { // all busDurationsWithX is the 'vertical' 1st column
+        if (busDurationsWithX === 'x') { continue; }
+        var currentTime = i + j;
+        var busDuration = busDurationsWithX[j];
+        if (busDuration - currentTime % busDuration !== 0)
+            continue allNumbers;
+        }
+        return currentTime;
+    }
+}
+
+console.log('Puzzle 2: ' + puzzleTwo());
