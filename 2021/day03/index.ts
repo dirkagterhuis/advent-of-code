eb2fa51557f12cffd6ac1223c347536bd3f1dd6d
Dirk Agterhuis <dirk.agterhuis@new10.com>
2021-12-03 20:38:41 +0100

    day 3 part 2. Sigh

--- 2021/day03/index.ts
+++ 2021/day03/index.ts
@@ -1,10 +1,22 @@
 import fs from 'fs'
 
-const sumsOfIndex: number[] = []
 let numberOfMeasurements = 0
+let data: number[][] = []
 fs.readFileSync('./input.txt', 'utf-8')
     .split(/[\r\n]+/g)
-    .map((inputElement) => {
+    .map((element) => {
+        data.push(
+            element.split('').map((str) => {
+                return parseInt(str, 10)
+            })
+        )
+    })
+
+// console.log(JSON.stringify(data))
+
+function checkMostOccuringBinary(inputArray: number[][]): number[] {
+    const sumsOfIndex: number[] = []
+    inputArray.map((inputElement) => {
         for (let i = 0; i < inputElement.length; i++) {
             inputElement[i]
             if (typeof sumsOfIndex[i] === 'undefined') {
@@ -15,10 +27,10 @@ fs.readFileSync('./input.txt', 'utf-8')
         }
         numberOfMeasurements++
     })
+    return sumsOfIndex
+}
 
-console.log('numberOfMeasurements: ' + numberOfMeasurements)
-console.log('sumsOfIndex: ' + sumsOfIndex)
-
+const sumsOfIndex = checkMostOccuringBinary(data)
 const sumsOfIndexResult: number[] = []
 const inverseSumOfIndexResult: number[] = []
 for (let i = 0; i < sumsOfIndex.length; i++) {
@@ -32,9 +44,6 @@ for (let i = 0; i < sumsOfIndex.length; i++) {
     }
 }
 
-console.log('sumsOfIndexResult: ' + sumsOfIndexResult)
-console.log('inverseSumOfIndexResult: ' + inverseSumOfIndexResult)
-
 const gammaRateBinary = sumsOfIndexResult.join('')
 const gammaRate = parseInt(gammaRateBinary, 2)
 const epsilonRateBinary = inverseSumOfIndexResult.join('')
@@ -43,5 +52,43 @@ const epsilonRate = parseInt(epsilonRateBinary, 2)
 console.log(
     `Part 1: Gamma Rate = ${gammaRate}, Epsilon Rate = ${epsilonRate}, Power consumption = ${
         gammaRate * epsilonRate
+    }`
+)
+
+let dataForOxygen = [...data]
+let dataForCO2 = [...data]
+for (let i = 0; i < sumsOfIndex.length; i++) {
+    let sumsOfIndexReducedOxygen = checkMostOccuringBinary(dataForOxygen)
+    const currentBinaryToFilterOnForOxyen =
+        sumsOfIndexReducedOxygen[i] >= dataForOxygen.length / 2 ? 1 : 0
+    if (dataForOxygen.length !== 1) {
+        dataForOxygen = filterOnBinaryOnK(dataForOxygen, currentBinaryToFilterOnForOxyen, i)
+    }
+
+    let sumsOfIndexReducedCO2 = checkMostOccuringBinary(dataForCO2)
+    const currentBinaryToFilterOnForCO2 = sumsOfIndexReducedCO2[i] >= dataForCO2.length / 2 ? 0 : 1
+    if (dataForCO2.length !== 1) {
+        dataForCO2 = filterOnBinaryOnK(dataForCO2, currentBinaryToFilterOnForCO2, i)
+    }
+}
+
+function filterOnBinaryOnK(arrayToFilter: number[][], binary: number, k: number): number[][] {
+    const ret = []
+    for (let l = 0; l < arrayToFilter.length; l++) {
+        if (arrayToFilter[l][k] === binary) {
+            ret.push(arrayToFilter[l])
+        }
+    }
+    return ret
+}
+
+const oxygenGeneratorRatingBinary = dataForOxygen[0].join('')
+const oxygenGeneratorRating = parseInt(oxygenGeneratorRatingBinary, 2)
+const co2RatingBinary = dataForCO2[0].join('')
+const co2Rating = parseInt(co2RatingBinary, 2)
+
+console.log(
+    `Part 2: oxygenGeneratorRating = ${oxygenGeneratorRating}, cO2ScrubberRating = ${co2Rating}, Life Support Rating = ${
+        oxygenGeneratorRating * co2Rating
     }`
 )
